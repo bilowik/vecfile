23e019cab8fd3784857276b5afae3e6875bd4952
Aaron Flores <aaronjflores962@gmail.com>
2019-10-08 11:07:57 -0400

    Fixed Doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
-pub mod vec_file;
-pub use crate::vec_file::*;
 /// A Vec-type collection that sits in a file vs in memory. Has many of the same operations as 
 /// Vec, and has some optional protections against Read/Write issues with the underlying file via
 /// 'shadows.' Can be iterated over, can be easily cloned, and converted to and from Vec. 
 ///
 /// ## Example
-/// ```rust
+/// ```
 /// use VecFile::*;
 ///
 /// fn main() {
@@ -15,3 +13,5 @@ pub use crate::vec_file::*;
 ///     assert_eq!(vf.pop(), 210);
 /// }
 /// ```
+pub mod vec_file;
+pub use crate::vec_file::*;
