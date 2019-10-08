aaee349f577ed2ef97f5236fb4f44c4058f17e68
Aaron Flores <aaronjflores962@gmail.com>
2019-10-08 10:51:18 -0400

    Added resize_with function

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 pub mod vec_file;
 pub use crate::vec_file::*;
+/// A Vec-type collection that sits in a file vs in memory. Has many of the same operations as 
+/// Vec, and has some optional protections against Read/Write issues with the underlying file via
+/// 'shadows.' Can be iterated over, can be easily cloned, and converted to and from Vec. 
+///
+/// ## Example
+/// ```rust
+/// use VecFile::*;
+///
+/// fn main() {
+///     let mut vf = VecFile::new();
+///     vf.push(&10u8);
+///     vf.push(&210u8);
+///     assert_eq!(vf.pop(), 210);
+/// }
+/// ```
