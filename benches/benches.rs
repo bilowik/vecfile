90840ab14e1b081987714bf26f95bdca11803624
Aaron Flores <aaronjflores962@gmail.com>
2019-10-11 10:48:06 -0400

    More benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -16,6 +16,43 @@ mod tests {
                 vf.push(val);
             }
         });
+
+        for val in &vf {
+            dbg!(val);
+        }
+
+    }
+
+    #[bench]
+    fn pop(b: &mut Bencher) {
+        const LEN: usize = 1024;
+        let buf: [u8; LEN] = unsafe { [std::mem::MaybeUninit::uninit().assume_init(); LEN] };
+        let mut vf = VecFile::new();
+        for val in buf.iter() {
+            vf.push(val);
+        }
+
+        b.iter(|| {
+            for _ in 0u64..(vf.len() - 1) {
+                vf.pop();
+            }
+        });
+    }
+
+    #[bench]
+    fn read(b: &mut Bencher) {
+        const LEN: usize = 1024;
+        let buf: [u8; LEN] = unsafe { [std::mem::MaybeUninit::uninit().assume_init(); LEN] };
+        let mut vf = VecFile::new();
+        for val in buf.iter() {
+            vf.push(val);
+        }
+
+        b.iter(|| {
+            for i in 0u64..vf.len() {
+                vf.get(i);
+            }
+        });
     }
 }
         
